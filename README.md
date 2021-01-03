ac84c14fce6aff72f9023de52953f46b119af136
skovati <skovati@protonmail.com>
2021-01-02 23:51:41 -0600

    add new photos

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Simple cryptocurrency tracker written in Go.
 Uses the [CoinGecko](https://www.coingecko.com/en/api) api to fetch real time prices and persistently stores the portfolio in json format.
 
-![](https://i.imgur.com/O8smJeW.gif)
+![](https://i.imgur.com/XfD2GO0.png)
+![](https://i.imgur.com/nJxhmgI.png)
 
 ### installation
 Unix
@@ -15,21 +16,12 @@ Gophers can build from source with
 ```
 make && sudo make install
 ```
-Windows users can run the exe found in the bin/ folder
-```
-& bin/kripto.exe
-```
-
 
 ### usage
 ```
-kripto init - Create a new portfolio
-kripto add - Use 'kripto add <currency> <amount>' to add a new crypto
-kripto remove - Use 'kripto remove <currency>' to remove
-kripto update - Use 'kripto update <currency> <new amount>' to update holdings
-kripto list - Lists current porfolio and holdings
-kripto help - Show this message
+kripto
 ```
+Ctrl-n and Ctrl-p for movement, Ctrl-c to escape
 
 ### uninstall
 ```
--- tui/addCoin.go
+++ tui/addCoin.go
@@ -1,24 +1,24 @@
 package tui
 
-import (
-    // "fmt"
+// import (
+//     // "fmt"
 
-    "github.com/gdamore/tcell/v2"
-    "github.com/rivo/tview"
-)
+//     "github.com/gdamore/tcell/v2"
+//     "github.com/rivo/tview"
+// )
 
-func AddCoin(nextSlide func()) (title string, content tview.Primitive) {
-    checkSupported := func(currency string, lastChar rune) bool {
-        supported, _ := GetCoinInfo(currency)
-        return supported
-    }
-    saveData := func() {
+// func AddCoin(nextSlide func()) (title string, content tview.Primitive) {
+//     checkSupported := func(currency string, lastChar rune) bool {
+//         supported, _ := GetCoinInfo(currency)
+//         return supported
+//     }
+//     saveData := func() {
         
-    }
-    form := tview.NewForm().
-        AddInputField("Enter coin name or ticker:", "", 20, checkSupported, nil).
-        AddInputField("Enter amount:", "", 10, nil, nil).
-        AddButton("Save", nextSlide).
-		AddButton("Cancel", nextSlide)
-}
+//     }
+//     form := tview.NewForm().
+//         AddInputField("Enter coin name or ticker:", "", 20, checkSupported, nil).
+//         AddInputField("Enter amount:", "", 10, nil, nil).
+//         AddButton("Save", nextSlide).
+// 		AddButton("Cancel", nextSlide)
+// }
 
